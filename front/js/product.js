062f82d7e5ceb9db6dafdb800e94e6d910e18937
Maxime Leblond <maxime.lbd@outlook.fr>
2022-06-26 20:45:51 +0200

    button save to cache ok

--- front/js/product.js
+++ front/js/product.js
@@ -1,16 +1,25 @@
-const url = new URLSearchParams(document.location.search).get('id');
+const id = new URLSearchParams(document.location.search).get('id');
+let numberPrice = 0;
+let altText;
+let imgUrl;
+let nameSofa;
 
-fetch(`http://localhost:3000/api/products/${url}`)
+fetch(`http://localhost:3000/api/products/${id}`)
   .then((response) => response.json())
   .then((data) => main(data));
 
 function main(data) {
-  const { _id, name, imageUrl, description, altTxt, price, colors } = data;
+  const { name, imageUrl, description, altTxt, price, colors } = data;
   itemImg(imageUrl, altTxt);
   document.querySelector('#title').textContent = name;
   document.querySelector('#price').textContent = price;
+  numberPrice = price;
   document.querySelector('#description').textContent = description;
   colorSelect(colors);
+  altText = altTxt;
+  imgUrl = imageUrl;
+  nameSofa = name;
+  buttonAddToCart();
 }
 
 function itemImg(imageUrl, altTxt) {
@@ -31,4 +40,32 @@ function colorSelect(colors) {
       select.appendChild(option);
     });
   }
+}
+
+function buttonAddToCart() {
+  const button = document.querySelector('#addToCart');
+  button.addEventListener('click', () => {
+    const color = document.querySelector('#colors').value;
+    const quantity = document.querySelector('#quantity').value;
+    if (
+      color === null ||
+      color === '' ||
+      quantity === null ||
+      quantity == '0'
+    ) {
+      alert('Veuillez choissir une quantité et une couleur');
+    } else {
+      const data = {
+        id: id,
+        color: color,
+        quantity: Number(quantity),
+        price: numberPrice,
+        imageUrl: imgUrl,
+        altTxt: altText,
+        name: nameSofa,
+      };
+      localStorage.setItem(id + '-' + color, JSON.stringify(data));
+      window.location.href = 'cart.html';
+    }
+  });
 }
